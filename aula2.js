ed6195db5d1573eb1316ce1d53ada563c21b1c0a
AshuraPK <168103263+AshuraPK@users.noreply.github.com>
2024-05-23 12:02:42 -0300

    aulas 2 de programação

---
+++ aula2.js
@@ -0,0 +1,39 @@
+var numero1 = 10
+var numero2 = 14
+
+var soma = numero1 + numero2
+var subtração = numero1 - numero2
+var multiplicacao = numero1 * numero2
+var divisao = numero1 / numero2
+
+console.log("soma dos valores:", soma)
+console.log("subtração de valores:", numero1 - numero2)
+console.log("multiplicação dos valores:", multiplicacao)
+console.log("divisão dos valores:", divisao)
+//
+// faça uma tabuada utilizando variaveis!
+// TABUADA DO 5
+//Control H, com esse recurso eu consigo mudar diversas variaveis - Também aperte o escrou para selecionar 
+
+var numeroEscolhido = 7
+
+console.log("Tabuada do 7!")
+
+console.log(numeroEscolhido,"x 0:", numeroEscolhido * 0)
+console.log 
+console.log(numeroEscolhido,"x 1:", numeroEscolhido * 1)
+console.log(numeroEscolhido,"x 2:", numeroEscolhido * 2)
+console.log(numeroEscolhido,"x 3:", numeroEscolhido * 3)
+console.log(numeroEscolhido,"x 4:", numeroEscolhido * 4)
+console.log(numeroEscolhido,"x 5:", numeroEscolhido * 5)
+console.log(numeroEscolhido,"x 6:", numeroEscolhido * 6)
+console.log(numeroEscolhido,"x 7:", numeroEscolhido * 7)
+console.log(numeroEscolhido,"x 8:", numeroEscolhido * 8)
+console.log(numeroEscolhido,"x 9:", numeroEscolhido * 9)
+console.log(numeroEscolhido,"x 10:", numeroEscolhido * 10)
+
+
+
+
+
+
